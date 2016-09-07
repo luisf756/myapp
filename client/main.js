5777a14c79c8a4f5df8e970f80805e7e2be7c345
luis munoz <luchof756@gmail.com>
2016-09-07 12:55:34 +0200

    add suma resta funcion

--- client/main.js
+++ client/main.js
@@ -15,8 +15,12 @@ Template.hello.helpers({
 });
 
 Template.hello.events({
-  'click button'(event, instance) {
+  'click #suma'(event, instance) {
     // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
+      instance.counter.set(instance.counter.get() + 1);
+  },
+  'click #resta'(event, instance) {
+    // increment the counter when button is clicked
+      instance.counter.set(instance.counter.get() - 1);
   },
 });
